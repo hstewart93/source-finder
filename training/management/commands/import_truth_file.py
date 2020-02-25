bb067c8bb90b4fbed4780b4ceefa2465c0fdba05
hstewart93 <harriet.stt@gmail.com>
2020-02-25 17:46:00 +0000

    Use multiprocessing to create db objects in parallel.

--- training/management/commands/import_truth_file.py
+++ training/management/commands/import_truth_file.py
@@ -1,8 +1,26 @@
-import csv
-
 from django.core.management.base import BaseCommand
+from multiprocessing import cpu_count, Pool
 
 from training.models import TrueSource
+
+
+MODEL_FIELDS = [
+    "source_id",
+    "ra_core",
+    "dec_core",
+    "ra_centroid",
+    "dec_centroid",
+    "flux",
+    "core_fraction",
+    "bmaj",
+    "bmin",
+    "position_angle",
+    "size",
+    "classification",
+    "selection",
+    "x_centroid",
+    "y_centroid",
+]
 
 
 class Command(BaseCommand):
@@ -18,34 +36,22 @@ class Command(BaseCommand):
         parser.add_argument("filepath", nargs="+", type=str)
         parser.add_argument("header_end", nargs="+", type=int)
 
+    @staticmethod
+    def create_objects(source):
+        """"""
+        items = dict(zip(MODEL_FIELDS, source.split()))
+        return TrueSource.objects.create(**items)
+
     def handle(self, *args, **options):
         """Method that reads in the file parsed as a command argument and creates
         TrueSource obejcts in the database from each row."""
         path = options["filepath"][0]
         if path.endswith(".txt"):
             with open(path, "r") as file:
-                sources = file.readlines()
+                lines = file.readlines()
+            sources = lines[options["header_end"][0] :]
 
-            for source in sources[options["header_end"][0] :]:
-                features = source.split()
-                model_fields = [
-                    "source_id",
-                    "ra_core",
-                    "dec_core",
-                    "ra_centroid",
-                    "dec_centroid",
-                    "flux",
-                    "core_fraction",
-                    "bmaj",
-                    "bmin",
-                    "position_angle",
-                    "size",
-                    "classification",
-                    "selection",
-                    "x_centroid",
-                    "y_centroid",
-                ]
+            pool = Pool(processes=cpu_count())
+            pool.map(self.create_objects, sources)
 
-                items = dict(zip(model_fields, features))
-                TrueSource.objects.create(**items)
             print(f"Created {TrueSource.objects.all().count()} TrueSource objects")
